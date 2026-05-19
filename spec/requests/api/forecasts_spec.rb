7ad4bdf5fde3f0650b7d54e9c0b1cebe8f39d8fb
fabio <fabioczamora@gmail.com>
2026-05-19 11:16:08 -0300

    [test] move stub request to the helper function

--- spec/requests/api/forecasts_spec.rb
+++ spec/requests/api/forecasts_spec.rb
@@ -76,37 +76,4 @@ RSpec.describe 'Api::Forecasts', type: :request do
       )
     end
   end
-
-  def stub_geolocation_request(zipcode:, latitude:, longitude:)
-    stub_request(:get, "https://nominatim.openstreetmap.org/search?country=Brazil&format=json&postalcode=#{zipcode}")
-      .to_return(
-        status: 200,
-        body: [{
-          'lat' => latitude,
-          'lon' => longitude,
-          'display_name' => '03407-000, St. ABC'
-        }].to_json,
-        headers: { 'Content-Type' => 'application/json' }
-      )
-  end
-
-  def stub_weather_request(latitude:, longitude:)
-    stub_request(:get, %r{api\.open-meteo\.com/v1/forecast})
-      .with do |request|
-        request.uri.query_values['latitude'] == latitude &&
-          request.uri.query_values['longitude'] == longitude
-      end
-      .to_return(
-        status: 200,
-        body: {
-          'current' => { 'temperature_2m' => 25.0, 'relative_humidity_2m' => 38, 'rain' => 0.00 },
-          'current_units' => { 'temperature_2m' => '°C' },
-          'daily' => {
-            'temperature_2m_min' => [20.0],
-            'temperature_2m_max' => [28.0]
-          }
-        }.to_json,
-        headers: { 'Content-Type' => 'application/json' }
-      )
-  end
 end
